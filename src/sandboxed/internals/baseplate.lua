8426fa16faa67b004b03fdff5eab7ae0963ddbf1
Qquaded <devcodednsa@gmail.com>
2025-12-20 21:43:58 +0300

    Physics test 1.0.9.2

--- src/sandboxed/internals/baseplate.lua
+++ src/sandboxed/internals/baseplate.lua
@@ -2,13 +2,6 @@ local baseplate = Instance.new("Part")
 baseplate.Position = Vector3.new(0, 0, 0)
 baseplate.Size = Vector3.new(1000, 4, 1000)
 baseplate.Color = Color3.new(0.2, 0.2, 0.2)
-baseplate.Name = "Baseplate"
+baseplate.Name = "Grid"
 baseplate.Anchored = true
 baseplate.Parent = Scene
-
-local SerializationService = game:GetService("SerializationService")
-print("buh 2")
-local success, result = pcall(function(...)
-	return SerializationService.SerializeInstancesAsync({ game, baseplate })
-end)
-warn(result)
